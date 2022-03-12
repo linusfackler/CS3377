8a8754bc4c5eeb4fe3c2836b151eaf15bb6ae7b7
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-03-11 18:31:07 -0600

    update

--- A2/main.c
+++ A2/main.c
@@ -3,11 +3,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-#define LINESIZE 16
-#include <string.h>
+#include <stdio.h>
 #include <iomanip>
 #include <iostream>
+
+#define LINESIZE 16
 
 using namespace std;
 
@@ -38,6 +38,8 @@ int main (int argc, char* argv[])
         cout << endl;
         int position;
         cout << "blockCount " << blockcount << endl;
+        lseek(fd, blockcount, SEEK_SET);
+        cout << "line size " << LINESIZE << endl << endl;
 
         for (int line = 0; line < LINESIZE; line++)
         {
@@ -45,7 +47,7 @@ int main (int argc, char* argv[])
             for (int column = 0; column < LINESIZE; column++)
             {
                 position = lseek(fd, 0, SEEK_CUR);
-                cout << setw(04) << position ++ space << " ";
+                cout << setw(04) << position << space << ' ';
                 write(fd, &space, 1);
             } 
             cout << endl;
@@ -53,22 +55,47 @@ int main (int argc, char* argv[])
 
         cout << endl << endl;
 
+        // each line of od outputs 16 characters
+        // so, to make the output diagonal, we will use 0, 17, 34, ...
+
         int argvlength = strlen(argv[argNo]);
         cout << "argNo       " << argNo         << endl;
         cout << "argv[argNo] " << argv[argNo]   << endl;
         cout << "argvlength  " << argvlength    << endl;
-        cout << "blockCount  " << blockdount    << endl;
+        cout << "blockCount  " << blockcount    << endl;
 
         // odd left to right
         if (argNo % 2 != 0)
         {
             for (int i = 0; i < argvlength; i++)
             {
-                cout << "lseek" << setw(06) << (blockcount + )
+                cout << "lseek" << setw(06) << (blockcount + (LINESIZE + 1) * i)
+                    << " argv[" << argNo << "][" << i << "] " << argv[argNo][i] << endl;
+                lseek(fd, (blockcount + (LINESIZE + 1) * i), SEEK_SET);
+                write(fd, &argv[argNo][i], 1);
+            }
+        }
+        // even left to right
+        
+        else
+        {
+            const int adjust = 15;
+            cout << "adjust " << adjust << endl;
+
+            for (int i = 1; i < argvlength + 1; i++)
+            {
+                cout << "lseek " << setw(06) << (blockcount + (adjust * i)) << " argv[" << argNo 
+                    << "][" << (i-1) << "] " << argv[argNo][i-1] << endl;
+                lseek(fd, (blockcount + (adjust * i)), SEEK_SET);
+                write(fd, &argv[argv][i-1], 1);
             }
         }
 
-
-
+        blockcount += 256;
+        cout << "blockCount " << blockcount << endl;
     }
+
+    close(fd);
+
+    puts("diagonal2.out has been created.\nUse od -Ad-c diagonal.out to see the contents.\n");
 }
