f58785eeca890dd5f1fbfc28d2b0f5df567a162d
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-03-26 14:42:09 -0500

    update

--- Fork/main.cpp
+++ Fork/main.cpp
@@ -10,3 +10,15 @@
 using namespace std;
 
 static const char* const QUOTES_FILE_NAME = "quotes.txt";
+
+int const READ      = 0;
+int const WRITE     = 1;
+int const PIPE_ERROR    = -1;
+int const CHILD_PID     =  0;
+int const MAX_PIPE_MESSAGE_SIZE     = 1000;
+int const MAX_QUOTE_LINE_SIZE       = 1000;
+
+void getQuotesArray(char *lines[], unsigned &noLines)
+{
+    
+}
