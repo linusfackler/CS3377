eb93f50f6a79b33843a961bd7c86738d768a3cd5
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-03-25 20:20:24 -0500

    update

--- Fork/main.cpp
+++ Fork/main.cpp
@@ -1,1 +1,12 @@
 #include <stdlib.h>
+#include <string.h>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
+
+using namespace std;
+
+static const char* const QUOTES_FILE_NAME = "quotes.txt";
